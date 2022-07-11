4bf2eda763ecfb35d7ba66c12e359b46c3a4f9ed
keifnao <pajamajaewon@gmail.com>
2022-07-10 20:17:08 -0400

    mindfulness module works

--- js/main.js
+++ js/main.js
@@ -2,30 +2,50 @@ import Timer from "./timer.js";
 import Mindfulness from "./mindfulness.js";
 
 
-// let reverse = true;
+let reverseTimer = true;
 
-// const timerButton = document.getElementById("timerButton");
+const timerButton = document.getElementById("timerButton");
 
-// if (timerButton.addEventListener)
-//     timerButton.addEventListener("click", buildTimer, false);
-// else if (timerButton.attatchEvent)
-//     timerButton.attachEvent("onclick", buildTimer);
-
-
-// function buildTimer() {
-
-//     if (reverse) {
-//         new Timer(document.querySelector(".module__timer"));
-//     } else {
-//         document.querySelector(".module__timer").textContent = "";
-//     }
-
-//     reverse = !reverse;
-// }
+if (timerButton.addEventListener)
+    timerButton.addEventListener("click", buildTimer, false);
+else if (timerButton.attatchEvent)
+    timerButton.attachEvent("onclick", buildTimer);
 
 
+function buildTimer() {
+
+    if (reverseTimer) {
+        new Timer(document.querySelector(".module__timer"));
+    } else {
+        document.querySelector(".module__timer").textContent = "";
+    }
+
+    reverseTimer = !reverseTimer;
+}
+
+let reverseMindful = true;
+
+const mindfulButton = document.getElementById("mindfulButton");
+
+if (mindfulButton.addEventListener)
+    mindfulButton.addEventListener("click", buildMindfulness, false);
+else if (mindfulButton.attatchEvent)
+    mindfulButton.attachEvent("onclick", buildMindfulness);
+
+
+function buildMindfulness() {
+
+    if (reverseMindful) {
+        new Mindfulness(document.querySelector(".module__mindfulness"));
+    } else {
+        document.querySelector(".mindfulness__container").textContent = "";
+        document.querySelector(".module__mindfulness").textContent = "";
+    }
+
+    reverseMindful = !reverseMindful;
+}
 
 
 
-new Mindfulness(document.querySelector(".module__mindfulness"));
+
 
--- js/mindfulness.js
+++ js/mindfulness.js
@@ -15,8 +15,6 @@ export default class Mindfulness {
             console.log("Got Clicked")
             this.el.start.classList.remove(`mindfulness--visible`)
             this.sequence()
-            this.el.start.classList.add(`mindfulness--visible`)
-            console.log("Finished!")
         })
     }
 
@@ -47,31 +45,35 @@ export default class Mindfulness {
             this.el.breatheIn.classList.remove(`mindfulness--visible`)
             this.breatheOut()
             console.log("Finished breathein")
+
+            setTimeout(() => {
+                this.el.breatheOut.classList.remove(`mindfulness--visible`)
+                this.el.start.classList.add(`mindfulness--visible`)
+                this.el.circle.classList.remove(`anim--breathe__out`)
+                console.log("Finished breatheout")
+                this.el.start.classList.add(`mindfulness--visible`)
+                this.el.circle.classList.remove(`mindfulness--visible`)
+            }, 7000)
         }, 7000)
-        
-        setTimeout(() => {
-            this.el.breatheOut.classList.remove(`mindfulness--visible`)
-            this.el.start.classList.add(`mindfulness--visible`)
-            console.log("Finished breatheout")
-        }, 7000)
-        
     }
 
     static getHTML() {
         return `
-        <span class="mindfulness mindfulness__frame mindfulness__frame--breathe-in">
-            <p>Breathe In</p>
-            <i class="fa-solid fa-down-left-and-up-right-to-center"></i>
-            </span>
-        <span class="mindfulness mindfulness__frame mindfulness__frame--breathe-out">
-            <p>Breathe Out</p>
-            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
-            </span>
-        <div class="mindfulness mindfulness__breathing-circle">
+        <div class="mindfulness__container">
+            <span class="mindfulness mindfulness__frame mindfulness__frame--breathe-in">
+                <p>Breathe In</p>
+                <i class="fa-solid fa-down-left-and-up-right-to-center"></i>
+                </span>
+            <span class="mindfulness mindfulness__frame mindfulness__frame--breathe-out">
+                <p>Breathe Out</p>
+                <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                </span>
+            <div class="mindfulness mindfulness__breathing-circle">
+            </div>
+            <button type="button" class="mindfulness mindfulness--start shrink-border mindfulness--visible" type="button">
+                Start Mindfulness Exercise
+            </button>
         </div>
-        <button type="button" class="mindfulness mindfulness--start shrink-border mindfulness--visible" type="button">
-            Start Mindfulness Exercise
-        </button>
         `;
     }
 
